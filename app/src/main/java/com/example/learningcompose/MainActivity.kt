ad0d59ddb642081aff4584fa24f208976d3d1dc7
kkendirci <kubrakendirci0@gmail.com>
2023-09-05 15:55:01 +0300

    Compleated - Modifiers - Android Jetpack Compose - Part 3

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -3,41 +3,47 @@ package com.example.learningcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.learningcompose.ui.theme.LearningComposeTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity:ComponentActivity(){
+    override fun onCreate(savedInstanceState:Bundle?){
         super.onCreate(savedInstanceState)
-        setContent {
-            LearningComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+        setContent{
+            Column(
+                modifier=Modifier
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                    .border(5.dp,Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp,Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp,Color.Black)
+                    .padding(10.dp)
+            ){
+                Text(
+                    text="Hello",modifier=Modifier
+                        .border(5.dp,Color.Yellow)
+                        .padding(5.dp)
+                        .offset(20.dp,20.dp)
+                        .border(10.dp,Color.Blue)
+                        .padding(10.dp)
+                )
+                Spacer(modifier=Modifier.height(50.dp))
+                Text(text="WORLD")
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LearningComposeTheme {
-        Greeting("Android")
     }
 }

4c6853300f5293686f707bdd721b357f7eae7855
kkendirci <kubrakendirci0@gmail.com>
2023-09-11 15:02:37 +0300

    Compleated - Making a Meditation UI With Jetpack Compose - Android Studio Tutorial

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -1,43 +1,30 @@
 package com.example.learningcompose
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.learningcompose.ui.theme.LearningComposeTheme
+import com.example.learningcompose.ui.HomeScreen
+import com.example.learningcompose.ui.theme.MeditationUIYouTubeTheme
+import com.google.android.material.badge.ExperimentalBadgeUtils
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearningComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MeditationUIYouTubeTheme {
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LearningComposeTheme {
-        Greeting("Android")
     }
 }

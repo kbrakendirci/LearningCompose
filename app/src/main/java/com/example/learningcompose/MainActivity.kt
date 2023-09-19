3dab0f04facf5985c33b2190042460cbc98a1ca2
kkendirci <kubrakendirci0@gmail.com>
2023-09-19 16:05:07 +0300

    Compleated - How to Do PROPER Permission Handling in Jetpack Compose

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -1,43 +1,98 @@
 package com.example.learningcompose
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.learningcompose.ui.theme.LearningComposeTheme
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.example.learningcompose.ui.theme.PermissionHandlingComposeTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
+
+@ExperimentalPermissionsApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearningComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+            PermissionHandlingComposeTheme {
+                val permissionsState = rememberMultiplePermissionsState(
+                    permissions = listOf(
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.CAMERA,
+                    )
+                )
+
+                val lifecycleOwner = LocalLifecycleOwner.current
+                DisposableEffect(
+                    key1 = lifecycleOwner,
+                    effect = {
+                        val observer = LifecycleEventObserver { _, event ->
+                            if(event == Lifecycle.Event.ON_START) {
+                                permissionsState.launchMultiplePermissionRequest()
+                            }
+                        }
+                        lifecycleOwner.lifecycle.addObserver(observer)
+
+                        onDispose {
+                            lifecycleOwner.lifecycle.removeObserver(observer)
+                        }
+                    }
+                )
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Greeting("Android")
+                    permissionsState.permissions.forEach { perm ->
+                        when(perm.permission) {
+                            Manifest.permission.CAMERA -> {
+                                when {
+                                    perm.hasPermission -> {
+                                        Text(text = "Camera permission accepted")
+                                    }
+                                    //status.shouldShowRationale is always false by default even when the application is installed for the first time or when app data is cleared.
+                                    perm.shouldShowRationale -> {
+                                        Text(text = "Camera permission is needed" +
+                                                "to access the camera")
+                                    }
+                                    perm.isPermanentlyDenied() -> {
+                                        Text(text = "Camera permission was permanently" +
+                                                "denied. You can enable it in the app" +
+                                                "settings.")
+                                    }
+                                }
+                            }
+                            Manifest.permission.RECORD_AUDIO -> {
+                                when {
+                                    perm.hasPermission -> {
+                                        Text(text = "Record audio permission accepted")
+                                    }
+                                    perm.shouldShowRationale -> {
+                                        Text(text = "Record audio permission is needed" +
+                                                "to access the camera")
+                                    }
+                                    perm.isPermanentlyDenied() -> {
+                                        Text(text = "Record audio permission was permanently" +
+                                                "denied. You can enable it in the app" +
+                                                "settings.")
+                                    }
+                                }
+                            }
+                        }
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LearningComposeTheme {
-        Greeting("Android")
     }
 }

fcd6a3249b13ba94ae6665f10f37beffb2bd99ba
kkendirci <kubrakendirci0@gmail.com>
2023-09-20 14:19:05 +0300

    Compleated -Clean Theming in Compose

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -4,25 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.learningcompose.ui.theme.ComposeCustomThemingTheme
 import com.example.learningcompose.ui.theme.LearningComposeTheme
+import com.example.learningcompose.ui.theme.LocalSpacing
+import com.example.learningcompose.ui.theme.spacing
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearningComposeTheme {
+            ComposeCustomThemingTheme{
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.padding(
+                        MaterialTheme.spacing.medium
+                    ),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LocalSpacing.current.medium
                 }
             }
         }
--- app/src/main/java/com/example/learningcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/learningcompose/ui/theme/Theme.kt
@@ -45,3 +45,22 @@ fun LearningComposeTheme(
         content = content
     )
 }
+
+@Composable
+fun ComposeCustomThemingTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
